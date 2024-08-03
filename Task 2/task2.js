c9914ce6873693064c8db8801d4a361ade489f56
pranavmaverick20 <pranavahuja20@gmail.com>
2024-08-03 11:06:16 +0530

    Finished Task 2

---
+++ Task 2/task2.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const app = express();
+
+//this method is to parse json values
+
+app.use(express.json());
+
+//this function is to generate password
+
+const passwordGenerate = (numbers, capital, special, len) => {
+    let str = "qwertyuiopasdfghjklzxcvbnm";
+    if (numbers) {
+        str += "1234567890";
+    }
+    if (capital) {
+        str += "QWERTYUIOPASDFGHJKLZXCVBNM";
+    }
+    if (special) {
+        str += "!@#$%^&*()_+-=[]{}|;:',.<>/?`~";
+
+    }
+    let pass = "";
+    for (i = 0; i < len; i++) {
+        pass += str[Math.floor(Math.random() * str.length)];
+    }
+    return pass;
+};
+
+// Using a Get request to generate password and return JSON response
+
+app.get('/api/password', (req, res) => {
+    try {
+        const { numbers, capital, special, len } = req.body;
+        res.status(200).json({success: true, Password: passwordGenerate(numbers, capital, special, Number(len))});
+    }
+    catch(err){
+        res.status(500).json({success:false,data: err.message});
+    }
+});
+
+app.listen(5000, () => {
+    console.log("Server is listening on port 5000...");
+})
